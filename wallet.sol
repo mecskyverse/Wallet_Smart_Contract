abc7636a6a3a46010fdae8bfb996d1513b8cf90d
Aakash <vermaaakash1000@gmail.com>
2022-06-29 15:02:07 +0530

    Modifier added for owner or allower checking

--- wallet.sol
+++ wallet.sol
@@ -10,25 +10,30 @@ contract walle_allot{
     function allowances(address _who,uint _amount) public {
         require(_checkOwner(),"you are not the owner");
         _amount = _amount*1 ether;
-        allowance[_this]=_amount;
+        allowance[_who]=_amount;
     }
     receive() external payable{}
 
-    function withdrawal(address payable _to,uint _amount) public{
-            _amount=_amount*1 ether;
-        if(msg.sender==owner){
-            require(_amount<=address(this).balance,"Not enough funds");
-            
-            _to.transfer(_amount);
+     modifier ownerorAllowed(uint _amount){
+            require(_checkOwner() || allowance[msg.sender]>=_amount , "You are not allowed");
+            _;
 
         }
-        else {
-            require(allowance[msg.sender]>=_amount,"Amount is more than your allowance");
+
+    function withdrawal(address payable _to,uint _amount) public ownerorAllowed{
+            _amount=_amount*1 ether;
+        if(!_checkOwner()){
+            require(_amount<=address(this).balance,"Not enough funds");
             
             allowance[msg.sender]-=_amount;
             _to.transfer(_amount);
+            }
+        
+            _to.transfer(_amount);
+            
+            
 
-        }
+        
     }
     function allowancebalance() public view returns(uint)
     {
