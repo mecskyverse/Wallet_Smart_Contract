bc42caf599fe3a4a10537dffbe3d37d5c7370c2b
Aakash <vermaaakash1000@gmail.com>
2022-08-20 22:38:18 +0530

    networks added

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,18 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork:"hardhat",
+  networks:{
+    rinkeby:{
+      url:
+      accounts:
+      chainId:
+    }
+    localhost:{
+      url:"http://127.0.0.1:8545",
+      chainId:31337,
+      //Thanks hardhat for providing the accounts
+    }
+  }
   solidity: "0.8.9",
 };
