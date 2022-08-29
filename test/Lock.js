ec24aa0498c8eb8834377ed20b9550f2e2f02cb6
Aakash <vermaaakash1000@gmail.com>
2022-08-29 22:12:29 +0530

    beforeEach test added

--- test/Lock.js
+++ test/Lock.js
@@ -1,2 +1,10 @@
 const { ethers } = require("hardhat");
 const { asser, expect } = require("chai");
+
+describe("Allowance", function () {
+  let allowance, allowanceFactory;
+  beforeEach(async function () {
+    allowanceFactory = await ethers.getContractFactory("Allowance");
+    allowance = await allowanceFactory.deploy();
+  });
+});
